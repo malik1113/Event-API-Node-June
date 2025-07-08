d7b765eea38019d4bcdbf8678106a3953cd540ab
glf30 <greyson.frazier@gmail.com>
2025-07-08 11:19:02 -0400

    Add calculations for available tickets and total price

--- routes/bookings/bookingController.js
+++ routes/bookings/bookingController.js
@@ -1,14 +1,41 @@
-const Booking = require("./bookingModel")
+const Booking = require("./bookingModel");
+// add getEventById function for ticket availability and pricing data
+const { getEventById } = require("../events/eventController");
+const Event = require("../events/eventModel");
 
 const createBooking = async (bookingData) => {
-    try {
-        const newBooking = await Booking.create(bookingData)
-        return newBooking
-    } catch (error) {
-        throw error;
-    }
-} 
+  try {
+    // utilize our existing controller to grab the event by its ID in order for us to calculate the price
+    // bookingData.event - id of the event we're looking for
+    const event = await getEventById(bookingData.event);
+
+    // event.availableTickets -= bookingData.quantity;
+    const newAvailableTickets = event.availableTickets - bookingData.quantity;
+
+    // console.log("NEW TICKETS: " + newAvailableTickets)
+
+    // bookingData.event - the event id
+    await Event.findByIdAndUpdate(bookingData.event, {
+      availableTickets: newAvailableTickets,
+    });
+
+    // copying everything from our booking data into a new object that we are free to modify
+    const calculatedBookingData = {
+      user: bookingData.user,
+      event: bookingData.event,
+      quantity: bookingData.quantity,
+      status: bookingData.status,
+      //totalPrice: our total price calculation
+      totalPrice: bookingData.quantity * event.price,
+    };
+
+    const newBooking = await Booking.create(calculatedBookingData);
+    return newBooking;
+  } catch (error) {
+    throw error;
+  }
+};
 
 module.exports = {
-    createBooking
-}
+  createBooking,
+};
